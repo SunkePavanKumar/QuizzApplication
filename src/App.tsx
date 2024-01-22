3d198ffac72fbd156e2dcaaf097eca9cf9d4f460
Pavan Kumar Sunke <pavan.1413489@gmail.com>
2024-01-22 21:19:47 +0530

    COM : Removed the unwanted boolean variable

--- src/App.tsx
+++ src/App.tsx
@@ -30,7 +30,6 @@ function App() {
       {quizData.length === 0 ? (
         <h1 className=" text-red-400 text-center mt-24">
           <button
-            disabled=""
             type="button"
             className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 inline-flex items-center"
           >
